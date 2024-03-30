f4a1ad3039f522b876131529ab95dce3fe796be9
jef.rodio <jef.rodio@gmail.com>
2024-03-30 17:34:59 -0500

    Added normalization option to rbf interpolator.

--- main/main.f90
+++ main/main.f90
@@ -6,10 +6,24 @@ program interpolation
   use helper_mod
   implicit none
 
-  real(c_double), dimension(:,:), allocatable :: box
-  real(c_double), dimension(:), allocatable :: scalar
+  interface
+    subroutine checkRBF(rbf, npts, pc, vals)
+      use iso_c_binding
+      use rbf_mod
+      implicit none
+      type(rbf_interp), intent(in) :: rbf
+      integer, intent(in) :: npts
+      real(c_double), dimension(:,:), allocatable, intent(out) :: pc
+      real(c_double), dimension(:), allocatable, intent(out) :: vals
+    end subroutine
+  end interface
+
+  integer :: npts
+  real(c_double), dimension(:,:), allocatable :: box, pc
+  real(c_double), dimension(:), allocatable :: scalar, vals
   type(scalar_field) :: field
-  type(rbf_interp) :: rbf
+  type(rbf_interp) :: rbf, rbf_normed
+
 
   write(stdout,*) "Starting interpolation test bed!"
 
@@ -29,8 +43,45 @@ program interpolation
   call write_points_wscalar("field.csv", field%get_vertices(), field%get_values())
 
   write(stdout,*) "Setup radial basis function interpolator....."
-  call rbf%init(field)
+  call rbf%init(field,norm=.false.)
+  call rbf_normed%init(field,norm=.true.)
+
+  write(stdout,*) "Test interpolators"
+  
+  npts = 11
+  call checkRBF(rbf, npts, pc, vals)
+  call write_points_wscalar("interped.csv", pc, vals)
+
+  call checkRBF(rbf_normed, npts, pc, vals)
+  call write_points_wscalar("interped-normed.csv", pc, vals)
 
 end program
 
+subroutine checkRBF(rbf, npts, pc, vals)
+  use iso_c_binding
+  use rbf_mod
+  implicit none
+  type(rbf_interp), intent(in) :: rbf
+  integer, intent(in) :: npts
+  real(c_double), dimension(:,:), allocatable, intent(inout) :: pc
+  real(c_double), dimension(:), allocatable, intent(out) :: vals
+  !
+  real(c_double), dimension(3) :: pt, step
+  integer :: i
+  !
+
+  if(allocated(pc)) deallocate(pc)
+  if(allocated(vals)) deallocate(vals)
+  allocate( pc(3,npts) )
+  allocate( vals(npts) )
+  pt = 0.0_c_double
+  step = 0.1_c_double
+  pt = pt - step
+  do i=1,npts
+    pt = pt + step
+    vals(i) = rbf%interp(pt)
+    pc(:,i) = pt
+  end do
+
+end subroutine
 
--- rbf/rbf.f90
+++ rbf/rbf.f90
@@ -38,6 +38,7 @@ module rbf_mod
     class(rbf_function), allocatable :: func
     type(scalar_field) :: field
     real(c_double), dimension(:), allocatable :: weights
+    logical :: norm = .false.
   contains
     procedure, pass :: init
     procedure, pass :: interp
@@ -48,11 +49,12 @@ module rbf_mod
 contains
 
 
-  subroutine init (this, field, func)
+  subroutine init (this, field, func, norm)
     ! dummy vars
     class(rbf_interp), intent(inout) :: this
     class(scalar_field), intent(in) :: field
     class(rbf_function), intent(in), optional :: func
+    logical, intent(in), optional :: norm
     ! local
     type(rbf_multiquadric) :: default
     !
@@ -63,6 +65,7 @@ contains
     else
       this%func = default
     end if
+    if (present(norm)) this%norm = norm
 
     call this%calc_weights()
   end subroutine init
@@ -74,8 +77,20 @@ contains
     real(c_double), dimension(3), intent(in) :: pt
     ! result
     real(c_double) :: val
+    ! local
+    integer(c_int) :: i
+    real(c_double) :: sum, fval
+    real(c_double), dimension(3) :: p2
     !
+    sum = 0._c_double
     val = 0._c_double
+    do i=1,this%field%count()
+      p2 = this%field%get_vertex(i)
+      fval = this%func%rbf( this%radius(pt,p2) )
+      val = val + this%weights(i)*fval
+      sum = sum + fval
+    end do
+    if (this%norm) val = val/sum
   end function interp
 
 
@@ -85,9 +100,11 @@ contains
     class(rbf_interp), intent(inout) :: this
     ! local
     integer(c_int) :: i, j
-    integer :: info, npts
-    real(c_double), dimension(3) :: p1, p2
+    ! Note that these are 'normal' fortran integers not c_int sized.
+    integer :: info, npts 
     integer, dimension(:), allocatable :: ipiv
+    real(c_double) :: sum
+    real(c_double), dimension(3) :: p1, p2
     real(c_double), dimension(:,:), allocatable :: rbf, rhs
     !
     npts = this%field%count()
@@ -96,21 +113,27 @@ contains
     allocate( ipiv(npts) )
     allocate( rbf(npts,npts) )
 
-    do i=1, npts
+    do i=1, npts      
       p1 = this%field%get_vertex(i)
 
+      sum = 0.0_c_double
       do j=1, npts
         p2 = this%field%get_vertex(j)
         rbf(i,j) = this%func%rbf( this%radius(p1,p2) )
-        !write(*,*) i, j, this%radius(p1,p2), rbf(i,j)
+        sum = sum + rbf(i,j)
       end do
 
       rhs(i,1) = this%field%get_value(i)
+      if (this%norm) rhs(i,1) = rhs(i,1)*sum
     end do
     
     ! Solve linear system RBF*weights = RHS
     call getrf(rbf, ipiv, info)
     call getrs(rbf, ipiv, rhs, info=info)
+    this%weights = rhs(:,1)
+    write(*,*)
+    write(*,*) "weights"
+    write(*,*) this%weights
   end subroutine calc_weights
 
   subroutine solve ()
