19678d54dbd70f64ac84736f4a74b663f29b2473
jef.rodio <jef.rodio@gmail.com>
2024-03-30 17:35:59 -0500

    Add a python script using SciPy to test implementations of various RBF kernels and options.

---
+++ scripts/check.py
@@ -0,0 +1,34 @@
+import scipy.interpolate as interp
+import csv
+import numpy as np
+
+def ReadCSV(filename):
+  field = None
+  with open(filename) as f:
+    reader = csv.DictReader(f, delimiter=',')
+    field = {'points' : [], 'values' : []}
+    for row in reader:
+      field['points'].append([row['x'], row['y'], row['z']])
+      field['values'].append(row['var1'])
+
+  # convert to numpy array
+  field['points'] = np.array(field['points'], dtype=float)
+  field['values'] = np.array(field['values'], dtype=float)
+  print(field)
+  return field
+
+def main():
+  # Read in field to interpolate over
+  field = ReadCSV('field.csv')
+  tests = ReadCSV('interped.csv')
+
+  # pass into radial basis function interpolator
+  rbf = interp.RBFInterpolator(field['points'],field['values'], 
+                               kernel='multiquadric', epsilon=1.0,
+                               degree=-1)
+  check = rbf(tests['points'])
+  check = np.array(check)
+  print(check)
+
+if __name__ == "__main__":
+  main()
